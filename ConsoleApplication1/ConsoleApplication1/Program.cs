eeeeaa0eda590061cf80497c6a49a05f65664d1d
s27475 <s27475@pjwstk.edu.pl>
2024-03-17 21:09:12 +0100

    Modyfikacja 1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace ConsoleApplication1
+﻿
+using System;
+
+namespace ConsoleApplication1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("test1");
         }
     }
 }
